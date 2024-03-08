3b0efa6ef9c1ec8f1571f80ab0c157977406a477
Ihsaan-20 <ahsaansindhi1@gmail.com>
2024-03-08 18:35:43 +0500

    "main"

--- app/Http/Controllers/InstallationController.php
+++ app/Http/Controllers/InstallationController.php
@@ -19,6 +19,7 @@ use Throwable;
 
 class InstallationController extends Controller {
     private $api_scopes, $api_key, $api_secret;
+
     public function __construct() {
         $this->api_scopes = implode(',', config('custom.api_scopes'));
         $this->api_key = config('custom.shopify_api_key');
@@ -102,98 +103,98 @@ class InstallationController extends Controller {
         }
     }
 
-    // public function saveStoreDetailsToDatabase($shopDetails, $accessToken) {
-    //     try {
-    //         $payload = [
-    //             'access_token' => $accessToken,
-    //             'myshopify_domain' => $shopDetails['myshopify_domain'],
-    //             'id' => $shopDetails['id'],
-    //             'email' => $shopDetails['email'],
-    //             'name' => $shopDetails['name'],
-    //             'phone' => $shopDetails['phone'],
-    //             'address1' => $shopDetails['address1'],
-    //             'address2' => $shopDetails['address2'],
-    //             'zip' => $shopDetails['zip']
-    //         ];
-    //         $store_db = Store::updateOrCreate(['myshopify_domain' => $shopDetails['myshopify_domain']], $payload); 
-    //         $random_password = '123456';
-    //         Log::info('Password generated '.$random_password); 
-    //         $user_payload = [
-    //             'email' => $shopDetails['email'],
-    //             'password' => bcrypt($random_password),
-    //             'store_id' => $store_db->table_id,
-    //             'name' => $shopDetails['name']
-    //             //'email_verified_at' => date('Y-m-d h:i:s')
-    //         ];
-    //         $user = User::updateOrCreate(['email' => $shopDetails['email']], $user_payload);
-    //         $user->markEmailAsVerified(); //To mark this user verified without requiring them to.
-    //         $user->assignRole('Admin');
-    //         foreach(config('custom.default_permissions') as $permission)
-    //             $user->givePermissionTo($permission);
-    //         ConfigureWebhooks::dispatch($store_db->table_id);
-    //         $this->registerForFulfillmentService($store_db);
-    //         Session::flash('success', 'Installation for your store '.$shopDetails['name'].' has completed and the credentials have been sent to '.$shopDetails['email'].'. Please login.');
-    //         //Create ur own mail handler here
-    //         //Send the credentials to the registered email address on Shopify.
-    //         //Mail::to($shopDetails['email'])->send(new InstallComplete($user_payload, $random_password));
-    //         return $store_db;
-    //     } catch(Exception $e) {
-    //         Log::info($e->getMessage().' '.$e->getLine());
-    //         return false;
-    //     }
-    // }
+    public function saveStoreDetailsToDatabase($shopDetails, $accessToken) {
+        try {
+            $payload = [
+                'access_token' => $accessToken,
+                'myshopify_domain' => $shopDetails['myshopify_domain'],
+                'id' => $shopDetails['id'],
+                'email' => $shopDetails['email'],
+                'name' => $shopDetails['name'],
+                'phone' => $shopDetails['phone'],
+                'address1' => $shopDetails['address1'],
+                'address2' => $shopDetails['address2'],
+                'zip' => $shopDetails['zip']
+            ];
+            $store_db = Store::updateOrCreate(['myshopify_domain' => $shopDetails['myshopify_domain']], $payload); 
+            $random_password = '123456';
+            Log::info('Password generated '.$random_password); 
+            $user_payload = [
+                'email' => $shopDetails['email'],
+                'password' => bcrypt($random_password),
+                'store_id' => $store_db->table_id,
+                'name' => $shopDetails['name']
+                //'email_verified_at' => date('Y-m-d h:i:s')
+            ];
+            $user = User::updateOrCreate(['email' => $shopDetails['email']], $user_payload);
+            $user->markEmailAsVerified(); //To mark this user verified without requiring them to.
+            $user->assignRole('Admin');
+            foreach(config('custom.default_permissions') as $permission)
+                $user->givePermissionTo($permission);
+            // ConfigureWebhooks::dispatch($store_db->table_id);
+            $this->registerForFulfillmentService($store_db);
+            Session::flash('success', 'Installation for your store '.$shopDetails['name'].' has completed and the credentials have been sent to '.$shopDetails['email'].'. Please login.');
+            //Create ur own mail handler here
+            //Send the credentials to the registered email address on Shopify.
+            //Mail::to($shopDetails['email'])->send(new InstallComplete($user_payload, $random_password));
+            return $store_db;
+        } catch(Exception $e) {
+            Log::info($e->getMessage().' '.$e->getLine());
+            return false;
+        }
+    }
 
-    // public function registerForFulfillmentService($store) {
-    //     try {
-    //         $endpoint = getShopifyURLForStore('fulfillment_services.json', $store->toArray());
-    //         $headers = getShopifyHeadersForStore($store->toArray());
-    //         $body = [
-    //             "fulfillment_service" => [
-    //                 "name" => config('custom.fulfillment_service_name'),
-    //                 "callback_url" => route('service_callback'),
-    //                 "inventory_management" => true,
-    //                 "tracking_support" => true,
-    //                 "fulfillment_orders_opt_in" => true,
-    //                 "requires_shipping_method" => true,
-    //                 "format" => "json"
-    //             ]
-    //         ];
-    //         $response = $this->makeAnAPICallToShopify('POST', $endpoint, null, $headers, $body);
-    //         $store->update(['fulfillment_service_response' => json_encode($response)]);
-    //         if(isset($response['statusCode']) && $response['statusCode'] == 201)
-    //             $store->update(['fulfillment_service' => true, 'fulfillment_orders_opt_in' => true]);
-    //         Log::info('Response received from shopify for fulfillment service creation ');
-    //         Log::info($response);
+    public function registerForFulfillmentService($store) {
+        try {
+            $endpoint = getShopifyURLForStore('fulfillment_services.json', $store->toArray());
+            $headers = getShopifyHeadersForStore($store->toArray());
+            $body = [
+                "fulfillment_service" => [
+                    "name" => config('custom.fulfillment_service_name'),
+                    "callback_url" => route('service_callback'),
+                    "inventory_management" => true,
+                    "tracking_support" => true,
+                    "fulfillment_orders_opt_in" => true,
+                    "requires_shipping_method" => true,
+                    "format" => "json"
+                ]
+            ];
+            $response = $this->makeAnAPICallToShopify('POST', $endpoint, null, $headers, $body);
+            $store->update(['fulfillment_service_response' => json_encode($response)]);
+            if(isset($response['statusCode']) && $response['statusCode'] == 201)
+                $store->update(['fulfillment_service' => true, 'fulfillment_orders_opt_in' => true]);
+            Log::info('Response received from shopify for fulfillment service creation ');
+            Log::info($response);
 
-    //     } catch(Exception $e) {
-    //         Log::info('FS '.$e->getMessage().' '.$e->getLine());
+        } catch(Exception $e) {
+            Log::info('FS '.$e->getMessage().' '.$e->getLine());
         
-    //     } catch(Throwable $e) {
-    //         Log::info('FS '.$e->getMessage().' '.$e->getLine());
-    //     }
-    // }
+        } catch(Throwable $e) {
+            Log::info('FS '.$e->getMessage().' '.$e->getLine());
+        }
+    }
 
-    // private function getShopDetailsFromShopify($shop, $accessToken) {
-    //     try {
-    //         $endpoint = getShopifyURLForStore('shop.json', ['myshopify_domain' => $shop]);
-    //         $headers = getShopifyHeadersForStore(['access_token' => $accessToken]);
-    //         $response = $this->makeAnAPICallToShopify('GET', $endpoint, null, $headers);
-    //         if($response['statusCode'] == 200) {
-    //             $body = $response['body'];
-    //             Log::info($body);
-    //             if(!is_array($body)) $body = json_decode($body, true);
-    //             return $body['shop'] ?? null;
-    //         } else {
-    //             Log::info('Response recieved for shop details');
-    //             Log::info($response);
-    //             return null;
-    //         }
-    //     } catch(Exception $e) {
-    //         Log::info('Problem getting the shop details from shopify');
-    //         Log::info($e->getMessage().' '.$e->getLine());
-    //         return null;
-    //     }
-    // }
+    private function getShopDetailsFromShopify($shop, $accessToken) {
+        try {
+            $endpoint = getShopifyURLForStore('shop.json', ['myshopify_domain' => $shop]);
+            $headers = getShopifyHeadersForStore(['access_token' => $accessToken]);
+            $response = $this->makeAnAPICallToShopify('GET', $endpoint, null, $headers);
+            if($response['statusCode'] == 200) {
+                $body = $response['body'];
+                Log::info($body);
+                if(!is_array($body)) $body = json_decode($body, true);
+                return $body['shop'] ?? null;
+            } else {
+                Log::info('Response recieved for shop details');
+                Log::info($response);
+                return null;
+            }
+        } catch(Exception $e) {
+            Log::info('Problem getting the shop details from shopify');
+            Log::info($e->getMessage().' '.$e->getLine());
+            return null;
+        }
+    }
 
     private function requestAccessTokenFromShopifyForThisStore($shop, $code) {
         try {
