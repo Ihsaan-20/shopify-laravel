61f537711f7cd057bd140563232d8d4416761e1f
Ihsaan-20 <ahsaansindhi1@gmail.com>
2024-03-09 11:33:17 +0500

    "main"

--- app/Traits/FunctionTrait.php
+++ app/Traits/FunctionTrait.php
@@ -13,27 +13,27 @@ trait FunctionTrait {
     }
     
     public function validateRequestFromShopify($request) {
-        return true;
-        // try {
-        //     $arr = [];
-        //     $hmac = $request['hmac'];
-        //     unset($request['hmac']);
-        //     foreach($request as $key => $value){
-        //         $key=str_replace("%","%25",$key);
-        //         $key=str_replace("&","%26",$key);
-        //         $key=str_replace("=","%3D",$key);
-        //         $value=str_replace("%","%25",$value);
-        //         $value=str_replace("&","%26",$value);
-        //         $arr[] = $key."=".$value;
-        //     }
-        //     $str = implode('&', $arr);
-        //     $ver_hmac =  hash_hmac('sha256',$str,config('custom.shopify_api_secret'), false);
-        //     return $ver_hmac === $hmac;
-        // } catch(Exception $e) {
-        //     Log::info('Problem with verify hmac from request');
-        //     Log::info($e->getMessage().' '.$e->getLine());
-        //     return false;
-        // }
+
+        try {
+            $arr = [];
+            $hmac = $request['hmac'];
+            unset($request['hmac']);
+            foreach($request as $key => $value){
+                $key=str_replace("%","%25",$key);
+                $key=str_replace("&","%26",$key);
+                $key=str_replace("=","%3D",$key);
+                $value=str_replace("%","%25",$value);
+                $value=str_replace("&","%26",$value);
+                $arr[] = $key."=".$value;
+            }
+            $str = implode('&', $arr);
+            $ver_hmac =  hash_hmac('sha256',$str,config('custom.shopify_api_secret'), false);
+            return $ver_hmac === $hmac;
+        } catch(Exception $e) {
+            Log::info('Problem with verify hmac from request');
+            Log::info($e->getMessage().' '.$e->getLine());
+            return false;
+        }
     }
     
 }
