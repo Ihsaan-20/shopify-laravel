b390268507bc6cec23936d694ea4287ce624b075
Ihsaan-20 <ahsaansindhi1@gmail.com>
2024-03-18 11:22:46 +0500

    "main"

--- resources/views/shopify/index.blade.php
+++ resources/views/shopify/index.blade.php
@@ -6,10 +6,6 @@
 <body>
     <h1>Welcome to Shopify App</h1>
     <h2>Shop Details:</h2>
-    <ul>
-        <li>Shop Name: {{ $shopData['shop']['name'] }}</li>
-        <li>Shop Domain: {{ $shopData['shop']['domain'] }}</li>
-        <!-- Add more shop details as needed -->
-    </ul>
+    
 </body>
 </html>
