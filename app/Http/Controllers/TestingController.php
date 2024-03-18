bf65faee05137cacb78e64b83ddd54743aad4f9f
Ihsaan-20 <ahsaansindhi1@gmail.com>
2024-03-18 11:25:40 +0500

    "main"

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 
 class TestingController extends Controller
 {
@@ -11,19 +13,18 @@ class TestingController extends Controller
     protected $apiSecret = '312be2ef4318c0d9c05e6e7d5975cf6a';
     protected $shopUrl = 'https://quickstart-eaa7987a.myshopify.com';
 
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
-
     public function index()
     {
-        // Authenticate with Shopify
+        // Authenticate with Shopify and get access token
         $accessToken = $this->getAccessToken();
+
+        if (!$accessToken) {
+            return $this->redirectToShopify();
+        }
 
         // Make API request to fetch shop data
         $client = new Client();
-        $response = $client->request('GET', "https://{$this->shopUrl}/admin/api/2024-01/shop.json", [
+        $response = $client->request('GET', "{$this->shopUrl}/admin/api/2024-01/shop.json", [
             'headers' => [
                 'X-Shopify-Access-Token' => $accessToken,
                 'Accept' => 'application/json',
@@ -35,25 +36,23 @@ class TestingController extends Controller
         return view('shopify.index', compact('shopData'));
     }
 
-
-
     protected function getAccessToken()
-{
-    // Check if the access token is stored in session
-    if (session()->has('access_token')) {
-        return session()->get('access_token');
+    {
+        // Check if the access token is stored in session
+        if (Session::has('access_token')) {
+            return Session::get('access_token');
+        }
+
+        return null;
     }
 
-    // Redirect the user to the authorization endpoint if the access token is not available
-    $shopUrl = 'https://quickstart-eaa7987a.myshopify.com';
-    $apiKey = '6caab75eb0bbcdb5d04f13c85b1d25e1';
-    $scopes = 'read_products,write_products'; // Specify the required scopes
-    $redirectUri = 'https://elementary-solutions.com/shopify_store/public/shopify/auth'; // Your app's redirect URI
+    protected function redirectToShopify()
+    {
+        $scopes = 'read_products,write_products'; // Specify the required scopes
+        $redirectUri = 'https://elementary-solutions.com/shopify_store/public/shopify/auth'; // Your app's redirect URI
 
+        $authorizationUrl = "{$this->shopUrl}/admin/oauth/authorize?client_id={$this->apiKey}&scope={$scopes}&redirect_uri={$redirectUri}";
 
-    $authorizationUrl = "https://{$shopUrl}/admin/oauth/authorize?client_id={$apiKey}&scope={$scopes}&redirect_uri={$redirectUri}";
-
-    return redirect()->away($authorizationUrl);
-}
-
+        return Redirect::away($authorizationUrl);
+    }
 }
