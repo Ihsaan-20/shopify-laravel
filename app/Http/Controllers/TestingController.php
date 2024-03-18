3e2dccedbd56b27326789841369f5cd90d8c5552
Ihsaan-20 <ahsaansindhi1@gmail.com>
2024-03-18 11:38:07 +0500

    "main"

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -9,14 +9,15 @@ use Illuminate\Support\Facades\Session;
 
 class TestingController extends Controller
 {
-    protected $apiKey = '6caab75eb0bbcdb5d04f13c85b1d25e1';
-    protected $apiSecret = '312be2ef4318c0d9c05e6e7d5975cf6a';
+    protected $apiKey = 'c2be734f4ce6e80f56e42ac02aaead3c'; // custom app key
+    protected $apiSecret = '239d30e79b32f28a81efc11a50d33820';
     protected $shopUrl = 'https://quickstart-eaa7987a.myshopify.com';
 
     public function index()
 {
     // Authenticate with Shopify and get access token
-    $accessToken = $this->getAccessToken();
+    // $accessToken = $this->getAccessToken();
+    $accessToken = "16|Q8LNr3dZrC0XS5ua2yQzxgBsuqbNqMd9SMz3uLTB";
 
     if (!$accessToken) {
         return $this->redirectToShopify();
